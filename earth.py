36a958297b62668f88163e0601cbec313cf4ced6
FrancoAmbrosini <162723096+FrancoAmbrosini@users.noreply.github.com>
2024-03-22 22:10:44 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,5 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    if x<y:print("The result of "+ x +" comes first in the dictionary than " +y)
+    else:print("The result of " + y +" comes first in the dictionary than " +x)
